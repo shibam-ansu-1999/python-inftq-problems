d32a1223aef11fd6098bfe43526ae307652018c9
Shibam Hazra <67412243+shibam-ansu-1999@users.noreply.github.com>
2020-08-25 08:58:42 +0530

    Add files via upload
    Write a recursive function, is_palindrome() to find out whether a string is a palindrome or
    not. The function should return true if it is a palindrome. Else it should return false.

---
+++ palindrome_recursion.py
@@ -0,0 +1,12 @@
+def palin(text):
+    if len(text)<=1:
+        print("Palindrome")
+    else:
+        if text[0]==text[-1]:
+            print(text[1:-1])
+            palin(text[1:-1])
+
+        else:
+            print("Not a palindrome")
+
+palin("malayalam")
