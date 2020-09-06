4a7ed64d93ee6f12370f367c33daa02427fdda42
Shibam Hazra <shibamhazra53@gmail.com>
2020-09-06 18:40:41 +0530

    Add files via upload
    Care hospital wants to know the medical specialty visited by the maximum number of
    patients. Assume that the patient id of the patient along with the medical specialty visited by
    the patient is stored in a list. The details of the medical specialties are stored in a dictionary as
    follows:
    {
    "P":"Pediatrics",
    "O":"Orthopedics",
    "E":"ENT
    }
    Write a function to find the medical specialty visited by the maximum number of patients and
    return the name of the specialty.
    
    
    
    Note:
    1. Assume that there is always only one medical specialty that is visited by the maximum
    number of patients.
    2. Perform case sensitive string comparison wherever necessary
    
    
    Sample Input Expected                                                                               Output
    [101,P,102,O,302,P,305,P]                                                                          Pediatrics
    [101,O,102,O,302,P,305,E,401,O,656,O]                                                    Orthopedics
    [101,O,102,E,302,P,305,P,401,E,656,O,987,E]                                                   ENT

---
+++ patient_problem.py
@@ -0,0 +1,29 @@
+def cal_spe_count(y):
+
+    spe_dict={"P":"Pediatrics","O":"Orthopedics","E":"ENT"}
+    count_dict={"P":0,"O":0,"E":0}
+
+    for i in range(1,len(y),2):
+        count_dict[y[i]]+=1
+    max_spe="P"
+    max_val=0
+    for s,v in count_dict.items():
+        if v>max_val:
+            max_val=v
+            max_sep=s
+    return spe_dict[max_sep]
+
+y=[]
+n=int(input("Enter the number of patients: "))
+for i in range(0,n*2):
+    if i%2==0:
+        m=input("Enter patient ID number: ")
+        y.append(m)
+    else:
+        m=input("Enter patient specialization (E/O/P): ")
+        m=m.upper()
+        y.append(m)
+
+print("Name of the speciality is: ",cal_spe_count(y))
+
+        
